bd1dcf144969fb5a65a9490335b97b1e04f42ee7
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-11-04 14:37:21 +0000

    add hltretrieve

--- test/migrateSchema.py
+++ test/migrateSchema.py
@@ -52,13 +52,11 @@ def getOldTrgData(dbsession,runnum):
     '''
     generate new trgdata_id for trgdata
     select cmslsnum,deadtime,bitname,trgcount,prescale from trg where runnum=:runnum and bitnum=0 order by cmslsnum;
-    select cmslsnum,bitnum,bitname,trgcount,deadtime,prescale from trg where runnum=:runnum order by cmslsnum
-    single out trgcount[0],bitname[0],prescale[0]
-    pack into blob bitname,trgcount,prescale
-    insert into trgdata trgdata_id,runnum,source,bitzeroname,bitnameblob
-    insert into lstrg trgdata_id,runnum,cmslsnum,deadtime,bitzerocount,bitzeroprescale,prescaleblob,trgcountblob
+    select cmslsnum,bitnum,trgcount,deadtime,prescale from trg where runnum=:runnum order by cmslsnum
+    output [bitnames,databuffer]
     '''
-    databuffer={} #{cmslsnum:[deadtime,bizeroname,bitzerocount,bitzeroprescale,bitnames,trgcountBlob,trgprescaleBlob]}
+    bitnames=''
+    databuffer={} #{cmslsnum:[deadtime,bizeroname,bitzerocount,bitzeroprescale,trgcountBlob,trgprescaleBlob]}
     dbsession.typeConverter().setCppTypeForSqlType('unsigned int','NUMBER(10)')
     dbsession.typeConverter().setCppTypeForSqlType('unsigned long long','NUMBER(20)')
     try:
@@ -130,11 +128,11 @@ def getOldTrgData(dbsession,runnum):
             bitname=cursor.currentRow()['bitname'].data()
             trgcount=cursor.currentRow()['trgcount'].data()        
             prescale=cursor.currentRow()['prescale'].data()
+            
             if bitnum==0 and counter!=0:
-                bitnames=','.join(bitnameList)
                 trgcountBlob=CommonUtil.packArraytoBlob(trgcountArray)
                 prescaleBlob=CommonUtil.packArraytoBlob(prescaleArray)
-                databuffer[previouslsnum].append(bitnames)
+                #databuffer[previouslsnum].append(bitnames)
                 databuffer[previouslsnum].append(trgcountBlob)
                 databuffer[previouslsnum].append(prescaleBlob)
                 bitnameList=[]
@@ -149,22 +147,16 @@ def getOldTrgData(dbsession,runnum):
         bitnames=','.join(bitnameList)
         trgcountBlob=CommonUtil.packArraytoBlob(trgcountArray)
         prescaleBlob=CommonUtil.packArraytoBlob(prescaleArray)
-        databuffer[cmslsnum].append(bitnames)
         databuffer[cmslsnum].append(trgcountBlob)
         databuffer[cmslsnum].append(prescaleBlob)
-        print CommonUtil.unpackBlobtoArray(databuffer[377][5],'l')
+        #print CommonUtil.unpackBlobtoArray(databuffer[377][4],'l')
         del qHandle
         dbsession.transaction().commit()
+        return [bitnames,databuffer]
     except Exception,e :
         dbsession.transaction().rollback()
         del dbsession
         raise Exception,'migrateSchema.getOldTrgData:'+str(e)
-    return databuffer
-
-    #coral.transaction().start(False)
-    #idgen=idDealer.idDealer(schema)
-    #trgid=idgen.generateNextIDForTable(trgtable) 
-    #coral.transaction().commit()
     
 def transferLumiData(dbsession,runnum):
     '''
@@ -177,16 +169,90 @@ def transferLumiData(dbsession,runnum):
 
 def getOldHLTData(dbsession,runnum):
     '''
-    generate new hltdata_id for hltdata
-    select cmslsnum,pathname,inputcount,acceptcount,prescale from hlt where runnum=:runnum order by cmslsnum
-    find npath
-    pack for the first cmslsnum into blob pathname
-    pack for each cmslsnum into blob inputcount,acceptcount,prescale,
-    insert into hlt_data hltdata_id, runnum,npath,pathnameblob
-    insert into lshlt hltdata_id,runnum,cmslsnum,prescaleblob,hltcountblob,acceptblob
+    select count(distinct pathname) from hlt where runnum=:runnum
+    select cmslsnum,pathname,inputcount,acceptcount,prescale from hlt where runnum=:runnum order by cmslsnum,pathname
+    [pathnames,databuffer]
     '''
-    pass
     
+    databuffer={} #{cmslsnum:[inputcountBlob,acceptcountBlob,prescaleBlob]}
+    dbsession.typeConverter().setCppTypeForSqlType('unsigned int','NUMBER(10)')
+    dbsession.typeConverter().setCppTypeForSqlType('unsigned long long','NUMBER(20)')
+    try:
+        npath=0
+        dbsession.transaction().start(True)
+        qHandle=dbsession.nominalSchema().newQuery()
+        n=oldSchemaNames()
+        qHandle.addToTableList(n.hlttable)
+        qHandle.addToOutputList('COUNT(DISTINCT PATHNAME)','npath')
+        qCondition=coral.AttributeList()
+        qCondition.extend('runnum','unsigned int')
+        qCondition['runnum'].setData(int(runnum))
+        qResult=coral.AttributeList()
+        qResult.extend('npath','unsigned int')
+        qHandle.defineOutput(qResult)
+        qHandle.setCondition('RUNNUM=:runnum',qCondition)
+        cursor=qHandle.execute()
+        while cursor.next():
+            npath=cursor.currentRow()['npath'].data()
+        del qHandle
+        #print 'npath ',npath
+        dbsession.transaction().start(True)
+        qHandle=dbsession.nominalSchema().newQuery()
+        n=oldSchemaNames()
+        qHandle.addToTableList(n.hlttable)
+        qHandle.addToOutputList('CMSLSNUM','cmslsnum')
+        qHandle.addToOutputList('PATHNAME','pathname')
+        qHandle.addToOutputList('INPUTCOUNT','inputcount')
+        qHandle.addToOutputList('ACCEPTCOUNT','acceptcount')
+        qHandle.addToOutputList('PRESCALE','prescale')
+        qCondition=coral.AttributeList()
+        qCondition.extend('runnum','unsigned int')
+        qCondition['runnum'].setData(int(runnum))
+        qResult=coral.AttributeList()
+        qResult.extend('cmslsnum','unsigned int')
+        qResult.extend('pathname','string')
+        qResult.extend('inputcount','unsigned int')
+        qResult.extend('acceptcount','unsigned int')
+        qResult.extend('prescale','unsigned int')
+        qHandle.defineOutput(qResult)
+        qHandle.setCondition('RUNNUM=:runnum',qCondition)
+        qHandle.addToOrderList('cmslsnum')
+        qHandle.addToOrderList('pathname')
+        cursor=qHandle.execute()
+        pathnameList=[]
+        inputcountArray=array.array('l')
+        acceptcountArray=array.array('l')
+        prescaleArray=array.array('l')
+        ipath=0
+        while cursor.next():
+            cmslsnum=cursor.currentRow()['cmslsnum'].data()
+            pathname=cursor.currentRow()['pathname'].data()
+            ipath+=1
+            inputcount=cursor.currentRow()['inputcount'].data()
+            acceptcount=cursor.currentRow()['acceptcount'].data()
+            prescale=cursor.currentRow()['prescale'].data()
+            pathnameList.append(pathname)
+            inputcountArray.append(inputcount)
+            acceptcountArray.append(acceptcount)
+            prescaleArray.append(prescale)
+            if ipath==npath:
+                pathnames=','.join(pathnameList)
+                inputcountBlob=CommonUtil.packArraytoBlob(inputcountArray)
+                acceptcountBlob=CommonUtil.packArraytoBlob(acceptcountArray)
+                prescaleBlob=CommonUtil.packArraytoBlob(prescaleArray)
+                databuffer[cmslsnum]=[inputcountBlob,acceptcountBlob,prescaleBlob]
+                pathnameList=[]
+                inputcountArray=array.array('l')
+                acceptcountArray=array.array('l')
+                prescaleArray=array.array('l')
+                ipath=0
+        del qHandle
+        dbsession.transaction().commit()
+    except Exception,e :
+        dbsession.transaction().rollback()
+        del dbsession
+        raise Exception,'migrateSchema.getOldTrgData:'+str(e)
+    return [pathnames,databuffer]
 
 def main():
     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),description="migrate lumidb schema",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -202,9 +268,17 @@ def main():
     if args.debug:
         msg=coral.MessageStream('')
         msg.setMsgVerbosity(coral.message_Level_Debug)
-    result=getOldTrgData(dbsession,runnumber)
+    trgresult=getOldTrgData(dbsession,runnumber)
+    hltresult=getOldHLTData(dbsession,runnumber)
     del dbsession
     del svc
+    print trgresult[0]
+    print len(trgresult[0].split(','))
+    print trgresult[1]
+    print '==========='
+    print hltresult[0]
+    print len(hltresult[0].split(','))
+    print hltresult[1]
     #print result
 if __name__=='__main__':
     main()
