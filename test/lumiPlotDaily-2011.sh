c34ebcf5df408f2b9284779ed9d392d6b18883a4
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2011-03-28 16:37:03 +0000

    split by energy

--- test/lumiPlotDaily-2011.sh
+++ test/lumiPlotDaily-2011.sh
@@ -7,34 +7,35 @@ operationdir="/afs/cern.ch/cms/lumi/www/plots/operation"
 physicsdir="/afs/cern.ch/cms/lumi/www/plots/physicscertified"
 publicresultdir="/afs/cern.ch/cms/lumi/www/publicplots"
 logpath="/afs/cern.ch/cms/lumi"
+
 logname="lumiPlotDaily-2011.log"
 logfilename="$logpath/$logname"
 dbConnectionString="oracle://cms_orcoff_prod/cms_lumi_prod"
 physicsselectionFile="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/StreamExpress/goodrunlist_json.txt"
-beamenergy="3.5e03"
+beamenergy="3500"
+beamfluctuation="0.2"
 beamstatus="stable"
-beamfluctuation="0.1"
 
 source /afs/cern.ch/cms/cmsset_default.sh;
 cd $workdir
 eval `scramv1 runtime -sh`
 touch $logfilename
 date >> $logfilename
-lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $overviewdir -beamstatus $beamstatus --withTextOutput total2011vstime >> $logfilename 
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $overviewdir -beamstatus $beamstatus -beamenergy $beamenergy -beamfluctuation $beamfluctuation --withTextOutput total2011vstime >> $logfilename 
 sleep 1
-lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $publicresultdir -beamstatus $beamstatus --withTextOutput total2011vstime >> $logfilename 
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $publicresultdir -beamstatus $beamstatus -beamenergy $beamenergy -beamfluctuation $beamfluctuation --withTextOutput total2011vstime >> $logfilename 
 sleep 1
-lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $overviewdir -beamstatus $beamstatus --withTextOutput perday2011 >> $logfilename 
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $overviewdir -beamstatus $beamstatus -beamenergy $beamenergy -beamfluctuation $beamfluctuation --withTextOutput perday2011 >> $logfilename 
 sleep 1;
-lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $publicresultdir -beamstatus $beamstatus --withTextOutput perday2011 >> $logfilename 
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $publicresultdir -beamstatus $beamstatus -beamenergy $beamenergy -beamfluctuation $beamfluctuation --withTextOutput perday2011 >> $logfilename 
 sleep 1;
-lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $operationdir --withTextOutput  instpeak2011vstime >> $logfilename 
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $operationdir --withTextOutput instpeak2011vstime >> $logfilename 
 sleep 1
 lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $publicresultdir --withTextOutput instpeak2011vstime >> $logfilename 
 sleep 1
-lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $operationdir -beamstatus $beamstatus --withTextOutput total2011vsfill >> $logfilename 
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $operationdir -beamstatus $beamstatus  -beamenergy $beamenergy -beamfluctuation $beamfluctuation --withTextOutput total2011vsfill >> $logfilename 
 sleep 1
-lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $operationdir -beamstatus $beamstatus --withTextOutput total2011vsrun >> $logfilename 
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $operationdir -beamstatus $beamstatus  -beamenergy $beamenergy -beamfluctuation $beamfluctuation --withTextOutput total2011vsrun >> $logfilename 
 #sleep 1
 #lumiPlotFiller.py -c $dbConnectionString -P $authdir -o $physicsdir -i $physicsselectionFile --withTextOutput physicsvstime >> $logfilename
 #sleep 1
